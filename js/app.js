b9cdc4b6a2f86953539e33369ae667a5953a5a81
Brock Stone <BrockStone@fullsail.edu>
2014-09-17 19:40:21 -0400

    added login functionality

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 angular.module('drop', ['ngRoute', 'firebase'])
  
-.value('fbURL', 'https://angularjs-projects.firebaseio.com/')
+// .value('fbURL', 'https://angularjs-projects.firebaseio.com/')
  
-.factory('drops', function($firebase, fbURL) {
-  return $firebase(new Firebase(fbURL)).$asArray();
-})
+// .factory('drops', function($firebase, fbURL) {
+//   return $firebase(new Firebase(fbURL)).$asArray();
+// })
  
 .config(function($routeProvider) {
   $routeProvider
     .when('/', {
-      controller:'ListCtrl',
+      controller:'loginCtrl',
       templateUrl:'templates/dropFeed.html'
     })
     .when('/edit/:projectId', {
@@ -18,43 +18,112 @@ angular.module('drop', ['ngRoute', 'firebase'])
     })
     .when('/new', {
       controller:'CreateCtrl',
-      templateUrl:'detail.html'
+      templateUrl:'templates/menu.html'
     })
     .otherwise({
       redirectTo:'/'
     });
 })
  
-.controller('ListCtrl', function($scope, Projects) {
-  $scope.projects = Projects;
-})
- 
-.controller('CreateCtrl', function($scope, $location, $timeout, Projects) {
-  $scope.save = function() {
-      Projects.$add($scope.project).then(function(data) {
-          $location.path('/');
-      });
+.controller('loginCtrl', function($scope, $rootScope, $location, $firebase, $firebaseSimpleLogin) {
+  $scope.test = function() {
+    $location.path('/new');
   };
+
+  //Firebase Reference 
+  var myRef = new Firebase("https://drop.firebaseio.com");
+
+  $scope.auth = new $firebaseSimpleLogin(myRef);
+
+ 
+
+
+    // Logs a user in with inputted provider
+    $scope.login = function(provider) {
+      $scope.auth.$login(provider);
+    };
+
+   
+
+    // Logs a user out
+    $scope.logout = function() {
+      $scope.auth.$logout();
+    };
+
+      // $scope.auth.createUser($scope.loginData.email, $scope.loginData.password, function(error, user) {
+      //   if (error === null) {
+      //     console.log("User created successfully:", user);
+      //   } else {
+      //     console.log("Error creating user:", error);
+      //   }
+      // });
+     
+
+    // Upon successful login, set the user object
+    $rootScope.$on("$firebaseSimpleLogin:login", function(event, user) {
+      $scope.user = user;
+
+      console.log($scope.user.displayName);
+      // $location.path('/new');
+      
+      // firebaseRef.child('users').child($scope.user.uid).set({
+      //       displayName: $scope.user.displayName,
+      //       provider: $scope.user.provider,
+      //       provider_id: $scope.user.id
+      //     });
+      //  $scope.closeLogin();
+
+    });
+
+    // Upon successful logout, reset the user object and clear cookies
+    $rootScope.$on("$firebaseSimpleLogin:logout", function(event) {
+      $scope.user = null;
+
+      // window.cookies.clear(function() {
+      //   console.log("Cookies cleared!");
+      // });
+    });
+
+    // Log any login-related errors to the console
+    $rootScope.$on("$firebaseSimpleLogin:error", function(event, error) {
+      console.log("Error logging user in: ", error);
+    });
+
+
+
+})
+
+ 
+
+  
+
+ 
+.controller('CreateCtrl', function($scope, $location, $timeout) {
+  // $scope.save = function() {
+  //     Projects.$add($scope.project).then(function(data) {
+  //         $location.path('/');
+  //     });
+  // };
 })
  
 .controller('EditCtrl',
-  function($scope, $location, $routeParams, Projects) {
-    var projectId = $routeParams.projectId,
-        projectIndex;
+  function($scope, $location, $routeParams) {
+  //   var projectId = $routeParams.projectId,
+  //       projectIndex;
  
-    $scope.projects = Projects;
-    projectIndex = $scope.projects.$indexFor(projectId);
-    $scope.project = $scope.projects[projectIndex];
+  //   $scope.projects = Projects;
+  //   projectIndex = $scope.projects.$indexFor(projectId);
+  //   $scope.project = $scope.projects[projectIndex];
  
-    $scope.destroy = function() {
-        $scope.projects.$remove($scope.project).then(function(data) {
-            $location.path('/');
-        });
-    };
+  //   $scope.destroy = function() {
+  //       $scope.projects.$remove($scope.project).then(function(data) {
+  //           $location.path('/');
+  //       });
+  //   };
  
-    $scope.save = function() {
-        $scope.projects.$save($scope.project).then(function(data) {
-           $location.path('/');
-        });
-    };
+  //   $scope.save = function() {
+  //       $scope.projects.$save($scope.project).then(function(data) {
+  //          $location.path('/');
+  //       });
+  //   };
 });
