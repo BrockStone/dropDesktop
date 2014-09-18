71f5514a563f459a91a2dc8601da99d1785527c5
Brock Stone <BrockStone@fullsail.edu>
2014-09-17 23:19:39 -0400

    added consol page for user interaction

--- js/app.js
+++ js/app.js
@@ -10,13 +10,13 @@ angular.module('drop', ['ngRoute', 'firebase'])
   $routeProvider
     .when('/', {
       controller:'loginCtrl',
-      templateUrl:'templates/dropFeed.html'
+      templateUrl:'templates/welcome_to_drop.html'
     })
     .when('/edit/:projectId', {
       controller:'EditCtrl',
       templateUrl:'detail.html'
     })
-    .when('/new', {
+    .when('/drop', {
       controller:'CreateCtrl',
       templateUrl:'templates/menu.html'
     })
@@ -24,31 +24,32 @@ angular.module('drop', ['ngRoute', 'firebase'])
       redirectTo:'/'
     });
 })
- 
+
+/////////////////////////
+
+
+    // LOGIN - LOGOUT
+    // Facebook + Twitter 
+
+
+/////////////////////////
+
 .controller('loginCtrl', function($scope, $rootScope, $location, $firebase, $firebaseSimpleLogin) {
-  $scope.test = function() {
-    $location.path('/new');
+  $scope.goToDrop = function() {
+    $location.path('/drop');
   };
 
   //Firebase Reference 
   var myRef = new Firebase("https://drop.firebaseio.com");
-
+  //Simple login
   $scope.auth = new $firebaseSimpleLogin(myRef);
 
- 
+  // Logs a user in with inputted provider
+  $scope.login = function(provider) {
+    $scope.auth.$login(provider);
+  };
 
-
-    // Logs a user in with inputted provider
-    $scope.login = function(provider) {
-      $scope.auth.$login(provider);
-    };
-
-   
-
-    // Logs a user out
-    $scope.logout = function() {
-      $scope.auth.$logout();
-    };
+  //Email and Password login ->
 
       // $scope.auth.createUser($scope.loginData.email, $scope.loginData.password, function(error, user) {
       //   if (error === null) {
@@ -57,47 +58,46 @@ angular.module('drop', ['ngRoute', 'firebase'])
       //     console.log("Error creating user:", error);
       //   }
       // });
+    
+  // Logs a user out
+  $scope.logout = function() {
+    $scope.auth.$logout();
+  };
      
+  // Upon successful login, set the user object
+  $rootScope.$on("$firebaseSimpleLogin:login", function(event, user) {
+    $scope.user = user;
 
-    // Upon successful login, set the user object
-    $rootScope.$on("$firebaseSimpleLogin:login", function(event, user) {
-      $scope.user = user;
+    console.log($scope.user.displayName);
+    // $location.path('/new');
+    
+    // firebaseRef.child('users').child($scope.user.uid).set({
+    //       displayName: $scope.user.displayName,
+    //       provider: $scope.user.provider,
+    //       provider_id: $scope.user.id
+    //     });
+    //  $scope.closeLogin();
 
-      console.log($scope.user.displayName);
-      // $location.path('/new');
-      
-      // firebaseRef.child('users').child($scope.user.uid).set({
-      //       displayName: $scope.user.displayName,
-      //       provider: $scope.user.provider,
-      //       provider_id: $scope.user.id
-      //     });
-      //  $scope.closeLogin();
+  });
 
-    });
+  // Upon successful logout, reset the user object and clear cookies
+  $rootScope.$on("$firebaseSimpleLogin:logout", function(event) {
+    $scope.user = null;
 
-    // Upon successful logout, reset the user object and clear cookies
-    $rootScope.$on("$firebaseSimpleLogin:logout", function(event) {
-      $scope.user = null;
+    // window.cookies.clear(function() {
+    //   console.log("Cookies cleared!");
+    // });
+  });
 
-      // window.cookies.clear(function() {
-      //   console.log("Cookies cleared!");
-      // });
-    });
-
-    // Log any login-related errors to the console
-    $rootScope.$on("$firebaseSimpleLogin:error", function(event, error) {
-      console.log("Error logging user in: ", error);
-    });
-
-
-
-})
+  // Log any login-related errors to the console
+  $rootScope.$on("$firebaseSimpleLogin:error", function(event, error) {
+    console.log("Error logging user in: ", error);
+  });
+}) // loginCtrl end
 
  
 
-  
 
- 
 .controller('CreateCtrl', function($scope, $location, $timeout) {
   // $scope.save = function() {
   //     Projects.$add($scope.project).then(function(data) {
